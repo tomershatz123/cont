02d4c6166d2a26c0cf1a01428d13a0ff2abb8547
Tomer Shatz <49619534+tomershatz123@users.noreply.github.com>
2019-04-15 18:02:52 +0300

    Delete telemetry.ts

--- telemetry.ts
+++
@@ -1,6 +0,0 @@
-export class Telemetry {
-    mmsi: number;
-    time: Date;
-    constructor(public received: Date, public lastLocation: Date, public reason: string, public gps: string,
-        public longitude: number, public latitude: number, public address: string) {}
-}
