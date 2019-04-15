54e76a4b48772e554483ab0b5cab4d0fe5ff08eb
Tomer Shatz <tomer.shatz@gmail.com>
2019-04-15 19:11:25 +0300

    n

---
+++ src/app/results/results.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { MatchService } from '../match.service';
+import { Telemetry } from '../telemetry';
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styleUrls: ['./results.component.css']
+})
+export class ResultsComponent implements OnInit {
+  results: Telemetry[] = [];
+  matchesObservable = null;
+  constructor(private router: Router, private matchService: MatchService) { }
+
+  ngOnInit() {
+    this.matchesObservable = this.matchService.getMatches();
+    // const matchesObservable = this.matchService.getMatches();
+    // matchesObservable.subscribe((matchesData: Telemetry[]) => {
+    //         this.results = matchesData;
+    // });
+  }
+
+  onClear() {
+    this.matchService.clear();
+    this.router.navigate(['/start']);
+  }
+}
---
+++ src/app/vessel.ts
@@ -0,0 +1,5 @@
+export class Vessel {
+
+    constructor(public mmsi: number, public time: Date, public longitude: number, public latitude: number) {
+    }
+}
