a34894f652756bbe473e77580e1a6ed151f316a1
Tomer Shatz <49619534+tomershatz123@users.noreply.github.com>
2019-04-15 18:07:31 +0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { ResultsComponent } from './results/results.component';
+
+const appRoutes: Routes = [
+  { path: 'start', component: StartComponent},
+  { path: '', redirectTo: '/start', pathMatch: 'full'},
+  { path: 'results', component: ResultsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
